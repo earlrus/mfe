21f0abe681c108510d093641ea83d5b97cca88eb
Aman <basilcrouch10@gmail.com>
2024-11-22 14:46:05 +0530

    logs added to test workflows"

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("hello aman");
+  console.log("hello aman !!");
 
   return (
     <div>
