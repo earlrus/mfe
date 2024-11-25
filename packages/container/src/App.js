bba4fa67cd9d37fbc6adc3c8c4e70286638bc4d9
Aman <basilcrouch10@gmail.com>
2024-11-25 13:30:15 +0530

    production ready randomize css class added #2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
-
+import MarketingApp from "./components/MarketingApp";
 import {
   StylesProvider,
   createGenerateClassName,
