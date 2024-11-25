4c85cf337acbf3c3c0acd59a283e0138238df93a
Aman <basilcrouch10@gmail.com>
2024-11-25 13:23:37 +0530

    production ready randomize css class added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 export default () => {
-  console.log("added logs to change the workflow");
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
 
   return (
-    <div>
-      <h1>Hello aman</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
